54793f18ff5c623ba63e5c776399e11da771c664
Ramin <r.ramazanpour@aut.ac.ir>
2023-11-19 06:47:24 +0300

    REMOVE LOGIN

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,10 @@
 import { ReactNode } from 'react';
-import { useAccount } from 'wagmi';
-import ConnectWallet from '@/components/ConnectWallet';
-import SignMessage from '@/components/SignMessage';
-import { isSSRMode } from '@/utils/helpers';
 
 const App = ({ children }: { children: ReactNode }) => {
-	const { address } = useAccount();
-	const token = isSSRMode() ? '' : localStorage.getItem('token');
-	if (!address) return <ConnectWallet />;
-	if (!token) return <SignMessage />;
+	// const { address } = useAccount();
+	// const token = isSSRMode() ? '' : localStorage.getItem('token');
+	// if (!address) return <ConnectWallet />;
+	// if (!token) return <SignMessage />;
 	return <>{children}</>;
 };
 
